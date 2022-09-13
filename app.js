8e2b6e25f2b17fe4ad6aba56be962985974f46f5
jonathan401 <jonathangomina5049@gmail.com>
2022-09-13 16:46:57 +0100

    redefine hex converter function

--- app.js
+++ app.js
@@ -1,81 +1,61 @@
-// dom variables
+console.log("trying out the hex color converter");
+// dom
 const form = document.querySelector("form");
 const copyBtn = document.querySelector(".btn");
 const colorBox = document.querySelector(".color-box");
 const hexField = document.querySelector("#hex");
 const rgbField = document.querySelector("#rgb");
+const errorMsg = document.querySelector(".error");
 
-const hexRegex = /^#([a-f0-9]{3}|[a-f0-9]{6})$/i;
+const hexRegex = /^#?(([a-f0-9]{3}|[a-f0-9]{6})$)/i;
 
-const toNum = (val) => {
-  return hexMap[val.toLowerCase()];
-};
+const toRGB = (val) => {
+  let rgb = [];
+  const result = hexRegex.test(val);
+  if (!result) {
+    return "not valid value";
+  }
 
-const toHex = (val1, val2) => {
-  return toNum(val1) * 16 + toNum(val2);
-};
-
-const hexMap = {
-  0: 0,
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  a: 10,
-  b: 11,
-  c: 12,
-  d: 13,
-  e: 14,
-  f: 15,
-};
-
-const toRGB = (string) => {
-  if (!hexRegex.test(string)) {
-    console.warn("that is not a valid hex value");
+  let hexCode = val.includes("#") ? val.split("#")[1].split("") : val.split("");
+  if (hexCode.length === 3) {
+    for (i = 0; i < hexCode.length; i++) {
+      /* duplicate values. i.e if hex === [1, 2, 3], make hex [11, 22, 33] */
+      const x = hexCode[i];
+      const colorVal = [x, x].join("");
+      rgb.push(parseInt(colorVal, 16));
+    }
   } else {
-    let hexNum = string.slice(1, string.length);
-    if (hexNum.length === 3) {
-      let [R, G, B] = hexNum.split(",").toString();
-      R = toHex(R, R);
-      G = toHex(G, G);
-      B = toHex(B, B);
-      rgbField.value = `rgb(${R}, ${G}, ${B})`;
-      copyBtn.classList.remove("disabled");
-      colorBox.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
-      return { R: R, G: G, B: B };
-    } else {
-      let [r1, r2] = hexNum.slice(0, 2).split(",").toString();
-      let [g1, g2] = hexNum.slice(2, 4).split(",").toString();
-      let [b1, b2] = hexNum.slice(4, hexNum.length).split(",").toString();
-
-      let R = toHex(r1, r2);
-      let G = toHex(g1, g2);
-      let B = toHex(b1, b2);
-      rgbField.value = `rgb(${R}, ${G}, ${B})`;
-      copyBtn.classList.remove("disabled");
-      colorBox.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
-      return { R: R, G: G, B: B };
+    for (i = 0; i < hexCode.length; i++) {
+      const [x, y] = [hexCode[i], hexCode[i + 1]];
+      const colorVal = [x, y].join("");
+      i += 1;
+      // convert to num to hex
+      rgb.push(parseInt(colorVal, 16));
     }
   }
+  const [r, g, b] = rgb;
+  copyBtn.classList.remove("disabled");
+  rgbField.value = `rgb(${r}, ${g}, ${b})`;
+  colorBox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
 copyBtn.addEventListener("click", () => {
   if (rgbField.value) {
     navigator.clipboard.writeText(rgbField.value);
     copyBtn.textContent = "copied to clipboard";
-    setTimeout(() => (copyBtn.textContent = "copy"), 2300);
+    setTimeout(() => (copyBtn.textContent = "copy"), 1900);
   }
 });
 
 hexField.addEventListener("keyup", () => {
   let result = hexField.value.trim();
-  if (hexRegex.test(result)) {
+  if (!hexRegex.test(result) && result.length) {
     // console.log(result);
+    hexField.classList.add("invalid");
+    errorMsg.textContent = `'${hexField.value}' is not a valid hex value`;
+  } else {
+    hexField.classList.remove("invalid");
     toRGB(result);
   }
 });
